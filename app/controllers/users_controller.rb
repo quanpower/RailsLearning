318b5c4178b09b337cb5fdc1cd85d7a7ca4f6910
quanpower <quanpower@gmail.com>
2014-10-19 22:08:27 +0800

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
+      redirect_to :back, :alert => "拒绝访问。"
     end
   end
 
