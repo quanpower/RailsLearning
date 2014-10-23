fab0f9e90ed2c7ad3065c6a3dcdabeb3068d912b
quanpower <quanpower@gmail.com>
2014-10-23 20:50:50 +0800

    edit db/migrate/devise_create_admin_user

--- db/migrate/20141023123939_devise_create_admin_users.rb
+++ db/migrate/20141023123939_devise_create_admin_users.rb
@@ -2,7 +2,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+    AdminUser.create!(email: 'quanpower@gmail.com', password: 'caojing1010', password_confirmation: 'caojing1010') if direction == :up
   end
 
   def change
