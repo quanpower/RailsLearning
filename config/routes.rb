59a30b68e81e2a7362bfafe1f8c9a14576864b58
quanpower <quanpower@gmail.com>
2014-10-23 20:45:20 +0800

    rails g active_admin install

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, ActiveAdmin::Devise.config
   resources :users
 end
