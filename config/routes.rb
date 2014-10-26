fe0b115d6b7413c61d9fc7002bd63445b7ff7ae0
quanpower <quanpower@gmail.com>
2014-10-26 13:23:04 +0800

    edit routes resource articles

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   resources :users
+  resources :articles
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
