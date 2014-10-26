c5e96e3e05ff183e48485a2d5a7f9480083a8c40
quanpower <quanpower@gmail.com>
2014-10-26 23:29:14 +0800

    add http_basic_authenticate to articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: 'quanpower', password: 'caojing1010',except: [:index, :show]
+
   def index
     @articles = Article.all
   end
