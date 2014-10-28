678871086d2bf5829c4e8af9822c191e7b279101
quanpower <quanpower@gmail.com>
2014-10-28 14:32:42 +0800

    drop table users;

--- db/migrate/20141027042719_add_column_to_users.rb
+++ db/migrate/20141027042719_add_column_to_users.rb
@@ -3,14 +3,13 @@ class AddColumnToUsers < ActiveRecord::Migration
     change_table(:users) do |t|
       t.string :name
       t.text :about
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email
+
       t.string   :authentication_token
       t.string   :image
       t.string   :location
       t.string   :username
     end
+
+    add_index :users, :authentication_token, unique: true
   end
 end
