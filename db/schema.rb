0784de9db6bc14b3bae391ed679684dc82ead23c
quanpower <quanpower@gmail.com>
2014-10-29 14:43:45 +0800

    create table user again

--- db/schema.rb
+++ db/schema.rb
@@ -74,4 +74,34 @@ ActiveRecord::Schema.define(version: 20141027042719) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.text     "about"
+    t.string   "authentication_token"
+    t.string   "image"
+    t.string   "location"
+    t.string   "username"
+  end
+
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
