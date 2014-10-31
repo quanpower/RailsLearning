87b5917cf856b5a31e67ed3f4ee42175b6d67580
quanpower <quanpower@gmail.com>
2014-11-01 01:52:52 +0800

    edit users_controller edit update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_filter :set_user, only: [:show, :edit, :update]
   before_filter :validate_authorization_for_user, only: [:edit, :update]
 
-
+  #GET /users/
+  def index
+    @users = User.all #否则报nil错误
+  end
   # GET /users/1
   def show
   end
@@ -14,7 +17,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
       render action: 'edit'
@@ -32,4 +35,8 @@ class UsersController < ApplicationController
     redirect_to root_path unless @user == current_user
   end
 
+  # STRONG params
+  def user_params
+    params.require(:user).permit(:name, :about)
+  end
 end
