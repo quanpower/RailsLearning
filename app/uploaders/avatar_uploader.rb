48c12fbf6d4cbec8851b4a68bd17fd018df57ea4
quanpower <quanpower@gmail.com>
2014-11-01 23:45:26 +0800

    edit uploaders/avatar_uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,46 +1,45 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick #使用minimagick处理压缩图片，确保安装imagemagick这个东东，ubuntu可以sudo apt-get install imagemagick
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
+  # Choose what kind of storage to use for this uploader: 
   storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
+  def store_dir  #定义上传到哪个文件夹下
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url  #可以定义默认图片，如过用户没有上传图片，则可以使用默认的图片
+    "avatar/#{version_name}.png"
+  end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+#图片的处理，有不同版本大小，网站可以在不同的地方调用不同的图片大小
+  version :normal do
+    process :resize_to_fill => [48, 48]
+  end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  version :small do
+    process :resize_to_fill => [16, 16]
+  end
+
+  version :large do
+    process :resize_to_fill => [64, 64]
+  end
+
+  version :big do
+    process :resize_to_fill => [120, 120]
+  end
+# Add a white list of extensions which are allowed to be uploaded.
+# For images you might use something like this:
+#指定上传文件的格式
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
