a3a2c8a1b7d5999a7e10d50fa496d4908b6ddd87
quanpower <quanpower@gmail.com>
2014-11-02 14:13:24 +0800

    add uploader: articleImage

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy #has many comments,dependent，依赖，删除文章会同时删除文章中的评论。
   validates :title, presence: true,
             length: { minimum: 5 }
+  mount_uploader :article_image, ArticleImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101154000) do
+ActiveRecord::Schema.define(version: 20141102043947) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141101154000) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "article_image"
   end
 
   create_table "authorizations", force: true do |t|
