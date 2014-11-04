6c0cdd2f16be171f8b1a14101f236fd32d1f15ae
quanpower <quanpower@gmail.com>
2014-11-04 23:12:26 +0800

    edit create_api_keys migration

--- db/migrate/20141104142437_create_api_keys.rb
+++ db/migrate/20141104142437_create_api_keys.rb
@@ -1,7 +1,23 @@
 class CreateApiKeys < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :api_keys do |t|
-      t.string :api_key
+      t.string :api_key, limit: 16
+      t.integer :channel_id
+      t.integer :user_id
+      t.boolean :write_flag, default: false
+      t.string :note
+
+      t.timestamps
     end
+
+    add_index :api_keys, :api_key, :unique => true
+    add_index :api_keys, :channel_id
+  end
+
+  def self.down
+    remove_index :api_keys, :channel_id
+    remove_index :api_keys, :api_key
+
+    drop_table :api_keys
   end
 end
