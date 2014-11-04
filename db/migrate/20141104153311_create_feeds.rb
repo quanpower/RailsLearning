8a614941b721972131e739e727a578a32fff8435
quanpower <quanpower@gmail.com>
2014-11-04 23:49:56 +0800

    edit create_feeds migration

--- db/migrate/20141104153311_create_feeds.rb
+++ db/migrate/20141104153311_create_feeds.rb
@@ -1,6 +1,28 @@
 class CreateFeeds < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :feeds do |t|
+      t.integer  :channel_id
+      t.string   :field1
+      t.string   :field2
+      t.string   :field3
+      t.string   :field4
+      t.string   :field5
+      t.string   :field6
+      t.string   :field7
+      t.string   :field8
+      t.integer  :entry_id
+      t.string   :status
+      t.decimal  :latitude,   precision: 15, scale: 10
+      t.decimal  :longitude,  precision: 15, scale: 10
+      t.string   :elevation
+      t.string   :location
+
+      t.timestamps
     end
+
+  end
+
+  def self.down
+    drop_table :feeds
   end
 end
