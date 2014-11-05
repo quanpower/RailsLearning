a2a1394c6e5b4e2ed4485ebf1d7d980bd2b58d00
quanpower <quanpower@gmail.com>
2014-11-05 22:22:22 +0800

    rails g migration CreateTags

---
+++ db/migrate/20141105141654_create_tags.rb
@@ -0,0 +1,15 @@
+class CreateTags < ActiveRecord::Migration
+  def up
+    create_table :tags do |t|
+      t.string :name
+
+      t.timestamps
+    end
+
+    add_index :tags, :name
+  end
+
+  def down
+    drop_table :tags
+  end
+end
