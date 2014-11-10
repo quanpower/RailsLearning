59ee8ac2b17c703ddb8282c479b7d64a7f98d50e
quanpower <quanpower@gmail.com>
2014-11-11 00:25:50 +0800

    edit channels_controller:add def index

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -146,6 +146,13 @@ class ChannelsController < ApplicationController
 
   end
 
-  
+  def index
+    @channels = current_user.channels
+    respond_to do |format|
+      format.html
+      format.json {render :json => @channels.not_social.to_json(Channel.private_options)}
+      format.xml { render :xml => @channel.not_social.to_xml(Channel.private_options)}
+    end
+  end
 
 end
