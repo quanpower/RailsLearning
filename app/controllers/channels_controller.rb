05ae847a5d4238747a14aefecf5f69d13c0a9ee6
quanpower <quanpower@gmail.com>
2014-11-10 23:17:16 +0800

    edit channels_controller:add def public

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -38,6 +38,44 @@ class ChannelsController < ApplicationController
 
     render :text => '1'
   end
-  
+
+  # list public channels
+  def public
+    # error if page 0
+    respond_with_error(:error_resource_not_found) and return if params[:page] == '0'
+
+    @domain = domain
+    # default blank response
+    @channels = Channel.where(:id => 0).paginate :page => params[:page]
+
+    # get channels by ids
+    if params[:channel_ids].present?
+      @header = "Channels"
+      @channels = Channel.public_viewable.by_array(params[:channel_ids]).order('ranking desc, updateed_at DESC').paginate :page => params[:page]
+    # get channels that match a user
+    elsif params[:username].present?
+      @header = "#{t(:user).capitalize}: #{params[:username]}"
+      searched_user = User.find_by_login(params[:username])
+      @channels = searched_user.channels.public_viewable.active.order('ranking desc, updated_at DESC').paginate :page => params[:page] if searched_user.present?
+    # get channels that match a tag
+    elsif params[:tag].present?
+      @header = "#{"tag".capitalize}: #{params[:tag]}"
+      @channels = Channel.public_viewable.active.order('ranking desc, updated_at DESC').with_tag(params[:tag]).paginate :page => params[:page]
+    # get channels by location
+    elsif params[:latitude].present? && params[:longitude].present? && params[:distance].present?
+      @header = "#{"Channels Near"}: [#{params[:latitude]}, #{params[:longitude]}]"
+      @channels = Channel.location_search(params).paginate :page => params[:page]
+    # normal channel list
+    else
+      @header = t(:featured_channels)
+      @channels = Channel.public_viewable.active.order('ranking desc, updated_at DESC').paginate :page => params[:page]
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => Channel.paginated_hash(@channels).to_json}
+      format.xml { render :xml => Channel.paginated_hash(@channels).to_xml(:root => 'response')}
+    end
+  end
 
 end
