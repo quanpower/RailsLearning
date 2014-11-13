dd68b91cc1549be226cd3f2ef4446117e19981c9
quanpower <quanpower@gmail.com>
2014-11-13 22:46:18 +0800

    edit controller comments edit def destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -45,14 +45,8 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
+    comment = current_user.comments.find(params[:id]).destroy
     redirect_to article_path(@article)
   end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
 end
