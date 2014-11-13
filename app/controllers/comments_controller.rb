345dd47f5f9a658605f2230d9481712949041c8b
quanpower <quanpower@gmail.com>
2014-11-13 19:14:08 +0800

    edit controller comments edit def index,create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,32 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: 'quanpower', password: 'caojing1010', only: :destroy
-  # 简单的http身份认证
+  before_filter :require_user
+
+  def index
+    redirect_to channel_path(:id => params[:channel_id], :public => true)
+  end
 
 
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    render :text => '' and return if params[:userlogin].length > 0
+
+    @channel = Channel.find(params[:channel_id])
+    @comment = @channel.comments.new
+    @comment.user = current_user
+    @comment.ip_address = get_header_value('X_REAL_IP')
+    @comment.parent_id = params[:parent_id]
+    @comment.body = params[:comment][:body].gsub(/<\/?[^>]*>/, '').gsub(/\n/, '<br /')
+
+    # save comment
+    if @comment.save
+      flash[:notice] = "Thanks for adding a comment!"
+    else
+      flash[:alert] = "Comment can't be blank!"
+    end
+    redirect_to :back
   end
+
+  
 
   def destroy
     @article = Article.find(params[:article_id])
