a1c9cfe92c9132695a41d4e4195bcc6afe0085c7
quanpower <quanpower@gmail.com>
2014-11-17 00:41:21 +0800

    edit status_controller add def index

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -20,5 +20,62 @@ class StatusController < ApplicationController
     end
   end
 
-  
+  def index
+    @channel = Channel.find(params[:channel_id])
+    @api_key = ApiKey.find_by_api_key(get_apikey)
+    @success = channel_permission?(@channel, @api_key)
+
+    # check for access
+    if @success
+      # create options hash
+      channel_options = { :only => channel_select_terse(@channel) }
+
+      # display only 1 day by default
+      params[:days] = 1 if !params[:days]
+
+      # set limits
+      limit = (request.format == 'csv') ? 1000000 :8000
+      limit = params[:results].to_i if (params[:results] and params[:results].to_i < 8000)
+
+      # get feed based on conditions
+      @feeds = @channel.feeds
+        .where(:created_at => get_date_range(params))
+        .select([:created_at, :entry_id, :status])
+        .order('created_at desc')
+        .limit(limit)
+
+      # sort properly
+      @feeds.reverse!
+
+      # set output correctly
+      if request.format == 'xml'
+        @channel_output = @channel.to_xml(channel_options).sub('</channel>', '').strip
+        @feed_output = @feeds.to_xml(:skip_instruct => true).gsub(/\n/, "\n  ").chop.chop
+      elsif request.format == 'csv'
+        @csv_headers = [:created_at, :entry_id, :status]
+        @feed_output = @feeds
+      else
+        @channel_output = @channel.to_json(channel_options).chop
+        @feed_output = @feeds.to_json
+      end
+    # else set error code
+    else
+      if params[:format] == 'xml'
+        @channel_output = bad_channel_xml
+      else
+        @channel_output = '-1'.to_json
+      end
+    end
+
+    # set callback for json
+    @callback = params[:callback] if params[:callback]
+
+    # output data in proper format
+    respond_to do |format|
+      format.html { render :template => 'feed/index'}
+      format.json { render :template => 'feed/index'}
+      format.xml { render :template => 'feed/index'}
+      format.csv { render :template => 'feed/index'}
+    end
+  end
 end
