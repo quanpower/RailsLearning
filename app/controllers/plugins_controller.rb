273c19489970f61c0064519a48742c790721b127
quanpower <quanpower@gmail.com>
2014-11-16 15:56:35 +0800

    edit plugins_controller add def public

--- app/controllers/plugins_controller.rb
+++ app/controllers/plugins_controller.rb
@@ -8,4 +8,25 @@ class PluginsController < ApplicationController
     @plugin = Plugin.find(params[:id])
     respond_with_error(:error_auth_required) and return if current_user.blank? || (@plugin.user_id != current_user.id)
   end
+
+  def new; ; end
+  def edit; ; end
+
+  # get list of public plugins
+  def public
+    # error if page 0
+    respond_with_error(:error_resource_not_found) and return if params[:page] == '0'
+
+    # default blank response
+    @plugins = Plugin.where(:id => 0).paginate :page => params[:page]
+
+    # get plugins
+    @plugins = Plugin.where("public_flag = true").order('updated_at DESC').paginate :page => params[:page]
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => Plugin.paginated_hash(@plugins).to_json}
+      format.xml { render :xml => Plugin.paginated_hash(@plugins).to_xml(:root => 'response')}
+    end
+  end
 end
