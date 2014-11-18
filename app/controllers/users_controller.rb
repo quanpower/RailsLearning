d3fc73faef4f7c6e9b4285717ac461b9cc1a5745
quanpower <quanpower@gmail.com>
2014-11-18 23:44:07 +0800

    edit users_controller add def api_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,3 +10,25 @@ class UsersController < ApplicationController
     flash[:notice] = t(:account_deleted)
     redirect_to root_path
   end
+
+  # allow login via api
+  def api_login
+    # get the user by login or email
+    user = User.find_by_login_or_email(params[:login])
+
+    # exit if no user or invalid password
+    respond_with_error(:error_auth_required) and return if user.blank? || !user.valid_password?(params[:password])
+
+    # save new authentication token
+    if user.authentication_token.blank?
+      user.authentication_token = Devise.friendly_token
+      user.save
+    end
+
+    # output the user with token
+    respond_to do |format|
+      format.json { render :json => user.as_json(User.private_options_plus(:authentication_token)) }
+      format.xml { render :xml => user.to_xml(User.private_options_plus(:authentication_token)) }
+      format.any { render :text => user.authentication_token }
+    end
+  end
