bd96d7ba95fbaa19151534adc83605b0e8259245
quanpower <quanpower@gmail.com>
2014-11-18 23:42:28 +0800

    edit users_controller add def destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,47 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :set_user, only: [:show, :edit, :update]
-  before_filter :validate_authorization_for_user, only: [:edit, :update]
+  include KeyUtilities
+  skip_before_filter :verify_authenticity_token, :only => [:api_login]
+  before_filter :require_user, :only => [:show, :edit, :update, :edit_profile]
 
-  #GET /users/
-  def index
-    @users = User.all #否则报nil错误
+  # delete account
+  def destroy
+    user = current_user
+    user.delete
+    flash[:notice] = t(:account_deleted)
+    redirect_to root_path
   end
-
-  def new
-    @user = User.new #否则报nil错误
-  end
-
-  # GET /users/1
-  def show
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update_attributes(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def validate_authorization_for_user
-    redirect_to root_path unless @user == current_user
-  end
-
-  # STRONG params
-  def user_params
-    params.require(:user).permit(:name, :about, :avatar, :avatar_cache)
-  end
-end
