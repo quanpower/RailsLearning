7d9fb2f5058df4091b8c5a264fd0243e64e8fef6
quanpower <quanpower@gmail.com>
2014-11-20 22:28:44 +0800

    edit feed_helper add def object_round

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -1,2 +1,12 @@
 module FeedHelper
+  include ApplicationHelper
+
+  # applies rounding to an enumerable object
+  def object_round(object, round=nil, match='field')
+    object.each_with_index do |o, index|
+      object[index] = item_round(o, round, match)
+    end
+
+    return object
+  end
 end
