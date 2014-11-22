ac2817be10cf71ac9cfb19d926e3a1a3b20784ad
quanpower <quanpower@gmail.com>
2014-11-22 21:31:04 +0800

    edit feed_helper add def get_floored_time

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -100,4 +100,12 @@ module FeedHelper
     return empty_clone
   end
 
+  # get the time floored to the correct number of seconds
+  def get_floored_time(input_time, seconds)
+    floored_seconds = (input_time.to_f / seconds).floor * seconds
+    # offset the seconds by the current time zone offset
+    offset_seconds = Time.zone.now.utc_offset
+    return Time.at(floored_seconds - offset_seconds)
+  end
+
 end
